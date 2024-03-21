adc0d1ed44ec7fbf0d436c5ce087934800026a4e
iak13cad <155821772+iak13cad@users.noreply.github.com>
2024-03-22 00:26:16 +0100

    Add files via upload

---
+++ average_grades/average_grade.py
@@ -0,0 +1,13 @@
+
+import csv
+
+def lab_average(filename, column):
+    #create file opening success checking here
+    #Set up file opening/closing here          
+    #implement the column calculation of all its row's lab scores here
+    pass    
+
+def main():
+    pass
+
+main()
