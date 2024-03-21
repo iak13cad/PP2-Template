f0484e6d59898e80964561a4eb88b5877393ce25
iak13cad <155821772+iak13cad@users.noreply.github.com>
2024-03-22 00:24:08 +0100

    Add files via upload

---
+++ word_tuples/word_tuples.py
@@ -0,0 +1,31 @@
+"""
+This questions has two parts: A and B.
+
+You have been provided with unit tests for each part in word_tuples_test.py.
+Do not alter any of the tests.  All tests should pass for your solution to be
+complete.
+
+Do a second version of the function which uses list comprehension instead.
+
+You may update the main function below for the purposes of manual testing and
+debugging.
+"""
+
+
+def make_word_tuples(word_list):
+   pass
+
+
+def letter_tuples_comprehension(word_list):
+    pass
+
+
+# For manual testing purposes, you may change as needed
+def main():
+    word_list = ["dog", "car", "it", "cake"]
+    word_tuple_list = make_word_tuples(word_list)
+    print("Word Tuples:", word_tuple_list)
+
+
+if __name__ == "__main__":
+    main()
