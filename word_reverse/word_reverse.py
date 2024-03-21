c034ef574c78f1dca4203f4f0c4d0d9fefdba067
iak13cad <155821772+iak13cad@users.noreply.github.com>
2024-03-22 00:24:34 +0100

    Add files via upload

---
+++ word_reverse/word_reverse.py
@@ -0,0 +1,12 @@
+
+def reverse_words(a_sentence):
+    pass
+
+
+def main():
+    reverse_words("This is a sentence.")
+    reverse_words("A very long sentence showing that this function works as intended and doesn't generate a runtime error.")
+    reverse_words("No")
+    reverse_words("")
+
+main()
