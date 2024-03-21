611b03ba57590c042a3fbccbe2b92a3a6632c411
iak13cad <155821772+iak13cad@users.noreply.github.com>
2024-03-22 00:25:32 +0100

    Add files via upload

---
+++ count_digits/count_digits.py
@@ -0,0 +1,14 @@
+def count_digits(filename):
+    # delete below line when coding
+    return None
+    # delete above line when coding
+
+def main():
+    count_digits("data/digits_1.txt")
+    count_digits("data/digits_10.txt")
+    count_digits("data/digits_100.txt")
+    count_digits("data/digits_100_bad.txt")
+    count_digits("data/not_a_file.txt")
+
+if __name__ == "__main__":
+    main()
